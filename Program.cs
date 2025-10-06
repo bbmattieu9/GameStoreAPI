fbada69dd3a73d85af18a97f1e3f2298d8607688
BbMattieu9 <bbmattieu9@gmail.com>
2025-10-06 16:23:30 +0100

    [Add] EFCore and Migrations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Data;
+using GameStore.Api.Endpoints;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStoreConnString");
+builder.Services.AddDbContext<GameStoreContext>(options => options.UseSqlServer(connString));
+
 var app = builder.Build();
+
+app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
